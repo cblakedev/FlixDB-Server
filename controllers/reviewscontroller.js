796605b28b0725ae97759ca3eed995bdfb97ead5
Steve Levy <stlevy002@sbcglobal.net>
2021-07-12 20:16:19 -0400

    Steve Branch

--- controllers/reviewscontroller.js
+++ controllers/reviewscontroller.js
@@ -6,6 +6,23 @@ let validateJWT = require('../middleware/jwt-validation');
 
 /* Write endpoint below */
 
-
+router.delete("/delete/:id", validateJWT, async (req, res) => {
+    const ownerID = req.user.id;
+    const reviewID = req.params.id;
+  
+    try {
+      const query = {
+        where: {
+          id: reviewID,
+          owner: ownerID
+        }
+      };
+  
+      await ReviewModel.destroy(query);
+      res.status(200).json({ messge: "Review Entry Removed"});
+    } catch (err) {
+      res.status(500).json({ error: err });
+    }
+  })
 
 module.exports = router
