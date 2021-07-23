544ef561f71bdf90a07952a9f2c9904fe0e69180
Chris Blake <c.blake@Chriss-Air.attlocal.net>
2021-07-22 22:39:56 -0400

    refactoring code

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize(process.env.DATABASE_URL || `postgresql://postgres:${encodeURIComponent(process.env.Pass)}@localhost:5432/movie-reviews`, {
+const sequelize = new Sequelize(process.env.DATABASE_URL || `postgresql://postgres:${encodeURIComponent(process.env.PASS)}@localhost:5432/movie-reviews`, {
     dialect: 'postgres',
     ssl: process.env.ENVIRONMENT === 'production'
 })
