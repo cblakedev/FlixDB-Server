f9b733a0f7eddd0e2659ac99788f09cb6b5c65fe
Brad Titus <bltitus@me.com>
2021-07-22 20:13:40 -0400

    troubleshooting heroku

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize("postgres://postgres:Corona14@localhost:5432/movie-reviews");
+const sequelize = new Sequelize("postgres://postgres:J4ck4ndD14n3!@localhost:5432/movie-reviews");
 
 module.exports = sequelize;
