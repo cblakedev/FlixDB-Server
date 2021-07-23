c960bd59d24ec7d29d955496a746cd2d5212b2e9
Chris Blake <c.blake@Chriss-Air.attlocal.net>
2021-07-22 22:33:23 -0400

    fixed bugs that was causing issues for deployment

--- db.js
+++ db.js
@@ -1,25 +1,31 @@
-require('dotenv').config();
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize(process.env.DATABASE_URL, process.env.HOST != 'local' ? {
+const sequelize = new Sequelize(process.env.DATABASE_URL || `postgresql://postgres:${encodeURIComponent(process.env.Pass)}@localhost:5432/movie-reviews`, {
     dialect: 'postgres',
-    define: {
-        timestamps: false
-    },
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false
-        }
-    }
-}
+    ssl: process.env.ENVIRONMENT === 'production'
+})
+    
+    
+    
+//     process.env.HOST != 'local' ? {
+//     dialect: 'postgres',
+//     define: {
+//         timestamps: false
+//     },
+//     dialectOptions: {
+//         ssl: {
+//             require: true,
+//             rejectUnauthorized: false
+//         }
+//     }
+// }
 
-    :
+//     :
 
-    {
-        dialect: 'postgres',
-        define: {
-            timestamps: false
-        }
-    }
-)
+//     {
+//         dialect: 'postgres',
+//         define: {
+//             timestamps: false
+//         }
+//     }
+// )
 module.exports = sequelize;
