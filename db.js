8a0efa175dcc0852252886abe12f025791e4b9c2
Brad Titus <bltitus@me.com>
2021-07-29 12:48:09 -0400

    update db.js

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize(process.env.DATABASE_URL || `postgresql://posgres:${encodeURIComponent(process.env.PASS)}@localhost/movie-reviews`, {
     dialect: 'postgres',
     ssl: process.env.ENVIRONMENT === 'production'
 });
